c451333c0786491eaa39934bc071d257438bc94d
FJanza <franciscojanza001@gmail.com>
2024-12-03 17:25:44 -0300

    add hitboys section

--- src/content/config.ts
+++ src/content/config.ts
@@ -146,5 +146,54 @@ const modoDiabloSongs = defineCollection({
     videoOwnerChannelTitle: z.string(),
   }),
 });
+const hitboySongs = defineCollection({
+  type: "data",
+  schema: z.object({
+    url: z.string(),
+    title: z.string(),
+    shortTitle: z.string(),
+    singers: z.string(),
+    date: z.string(),
+    thumbnails: z.object({
+      default: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      medium: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      high: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      standard: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      maxres: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+    }),
+    position: z.number(),
+    videoOwnerChannelTitle: z.string(),
+  }),
+});
 
-export const collections = {songs, freestyles, modoDiabloSongs};
+export const collections = {songs, freestyles, modoDiabloSongs, hitboySongs};
