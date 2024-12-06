3d6d502850ebbefd8f5dffa2e37932bea746f230
fjanza <franciscojanza001@gmail.com>
2024-12-06 01:13:03 -0300

    <feat>[dev] add vampiros, fix descentrado de scroll cuando buscabamos temas por fecha

--- src/content/config.ts
+++ src/content/config.ts
@@ -146,6 +146,7 @@ const modoDiabloSongs = defineCollection({
     videoOwnerChannelTitle: z.string(),
   }),
 });
+
 const hitboySongs = defineCollection({
   type: "data",
   schema: z.object({
@@ -196,4 +197,60 @@ const hitboySongs = defineCollection({
   }),
 });
 
-export const collections = {songs, freestyles, modoDiabloSongs, hitboySongs};
+const vampirosSongs = defineCollection({
+  type: "data",
+  schema: z.object({
+    url: z.string(),
+    title: z.string(),
+    shortTitle: z.string(),
+    singers: z.string(),
+    date: z.string(),
+    thumbnails: z.object({
+      default: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      medium: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      high: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      standard: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+      maxres: z
+        .object({
+          url: z.string(),
+          width: z.number(),
+          height: z.number(),
+        })
+        .optional(),
+    }),
+    position: z.number(),
+    videoOwnerChannelTitle: z.string(),
+  }),
+});
+
+export const collections = {
+  songs,
+  freestyles,
+  modoDiabloSongs,
+  hitboySongs,
+  vampirosSongs,
+};
