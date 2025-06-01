949530b34d975ff1a29417f1303fec7278d8e9c5
fjanza <franciscojanza001@gmail.com>
2025-05-31 21:28:57 -0300

    add Pauli interview and TheLastSong, and any fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,28 @@ export const convertToEmbed = (
 
   return null; // Si el enlace no es válido
 };
+
+/**
+ * Get how much time has passed since startDate date in spanish
+ * @param startDate
+ * @returns  string
+ */
+export const getTimeAgo = (startDate: Date): string => {
+  const now = new Date();
+  const diffInSeconds = Math.floor(
+    (now.getTime() - startDate.getTime()) / 1000
+  );
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  const diffInDays = Math.floor(diffInHours / 24);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} segundos`;
+  } else if (diffInMinutes < 60) {
+    return `${diffInMinutes} minutos`;
+  } else if (diffInHours < 24) {
+    return `${diffInHours} horas`;
+  } else {
+    return `${diffInDays} días`;
+  }
+};
